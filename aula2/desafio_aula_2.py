e1f10a3f235021af57135404d9880091e113d0b2
matheuxneves <matheusvieirab.n.02@gmail.com>
2025-02-23 23:43:35 +0000

    feat: commit inicial

---
+++ aula2/desafio_aula_2.py
@@ -0,0 +1,13 @@
+# 1. Definir as listas x e y
+
+# 2. Calcular as médias de x e y
+
+# 3. Inicializar as variáveis para os somatórios
+
+# 4. Utilizar um loop para calcular:
+#    - A soma de (x_i - média_x) * (y_i - média_y)
+#    - A soma de (x_i - média_x)²
+
+# 5. Calcular beta1 e beta0 usando as fórmulas dos mínimos quadrados
+
+# 6. Imprimir os resultados
